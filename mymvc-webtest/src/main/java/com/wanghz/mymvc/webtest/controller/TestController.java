8ceda3dfd3645839142ab386b8e2110ac9643178
wanghuzhou <943449628@qq.com>
2022-04-21 22:41:04 +0800

    fix: 修复表单参数在经过RequestBodyFilter消失的问题

--- mymvc-webtest/src/main/java/com/wanghz/mymvc/webtest/controller/TestController.java
+++ mymvc-webtest/src/main/java/com/wanghz/mymvc/webtest/controller/TestController.java
@@ -3,6 +3,7 @@ package com.wanghz.mymvc.webtest.controller;
 import com.wanghz.mymvc.annotation.Controller;
 import com.wanghz.mymvc.annotation.Qualifier;
 import com.wanghz.mymvc.annotation.RequestMapping;
+import com.wanghz.mymvc.common.util.ReflectUtil;
 import com.wanghz.mymvc.domain.ResponseBean;
 import com.wanghz.mymvc.webtest.dao.UserMapper;
 import com.wanghz.mymvc.webtest.entity.User;
@@ -10,6 +11,9 @@ import com.wanghz.mymvc.webtest.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/test")
@@ -47,5 +51,29 @@ public class TestController {
     public ResponseBean test1(User user, String city) {
         user.setCity(city);
         return ResponseBean.ofSuccess(user);
+    }
+
+    @RequestMapping("/getReqInfo")
+    public Map<String, Object> test2(HttpServletRequest request) {
+
+        Map<String, Object> map = new HashMap<>();
+        Map<String, String> header = new HashMap<>();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headName = headerNames.nextElement();
+            header.put(headName, request.getHeader(headName));
+        }
+        map.put("header", header);
+
+        Map<String, String> form = new HashMap<>();
+        Enumeration<String> formNames = request.getParameterNames();
+        while (formNames.hasMoreElements()) {
+            String headName = formNames.nextElement();
+            form.put(headName, request.getParameter(headName));
+        }
+        map.put("formBody", form);
+        map.put("json", ReflectUtil.getReqBodyString(request));
+
+        return map;
     }
 }
