38f50197eed13f9012c658ae96db37f1da09b9b7
wanghuzhou <943449628@qq.com>
2022-04-24 21:00:28 +0800

    移除druid

--- mymvc-webtest/src/main/java/com/wanghz/mymvc/webtest/config/DruidDataSourceFactory.java
+++ mymvc-webtest/src/main/java/com/wanghz/mymvc/webtest/config/DruidDataSourceFactory.java
@@ -1,6 +1,6 @@
 package com.wanghz.mymvc.webtest.config;
 
-import com.alibaba.druid.pool.DruidDataSource;
+//import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.ibatis.datasource.unpooled.UnpooledDataSourceFactory;
 
 import java.util.Properties;
@@ -8,7 +8,7 @@ import java.util.Properties;
 /**
  * druid连接池工厂
  */
-public class DruidDataSourceFactory extends UnpooledDataSourceFactory {
+/*public class DruidDataSourceFactory extends UnpooledDataSourceFactory {
     public DruidDataSourceFactory() {
         this.dataSource = new DruidDataSource();
     }
@@ -16,4 +16,4 @@ public class DruidDataSourceFactory extends UnpooledDataSourceFactory {
     public void setProperties(Properties properties) {
         ((DruidDataSource) this.dataSource).configFromPropety(properties);
     }
-}
+}*/
